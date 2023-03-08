6041f392fee88890c86fd3badc2e8cfdd5874e6f
Jose Blanco Fleury <92124689+josefleuryy@users.noreply.github.com>
2023-03-08 22:55:45 +0100

    Update HundirLaFlota.cpp

--- HundirLaFlota.cpp
+++ HundirLaFlota.cpp
@@ -42,7 +42,7 @@ void crearBarco12(int tablero[6][6], int fila, int columna){
     tablero[fila+1][columna]=1;
 }
 
-bool disparo(int tablero[6][6], int disparos[6][6]){
+bool disparo(int tablero[6][6]){
     bool quedanBarcos = false;
     
     int fila, columna;
@@ -98,7 +98,48 @@ int main(){
     int disparos[6][6];
     
     //Creamos una variable aleatoria
-    int aleatorio = 2;
+    int Aleatorio3x1_1x3;
+    int Aleatorio2x1_1x2;
+    int Aleatorio1x2_2x1; 
+
+    //Variables filasbarco y columnasbarco
+    int filaBarco3x1;
+    int columnaBarco3x1;
+
+    int filaBarco1x3;
+    int columnaBarco1x3;
+
+    int filaBarco2x1_1;
+    int columnaBarco2x1_1;
+
+    int filaBarco1x2_1;
+    int columnaBarco1x2_1;
+
+    int filaBarco2x1_2;
+    int columnaBarco2x1_2;      
+
+    int filaBarco1x2_2;
+    int columnaBarco1x2_2;
+
+    //Randomificamos las variables
+    filaBarco3x1 = rand()%6;
+    columnaBarco3x1 = rand()%4;
+
+    filaBarco1x3 = rand()%4;
+    columnaBarco1x3 = rand()%6;
+
+    filaBarco2x1_1 = rand()%6;
+    columnaBarco2x1_1 = rand()%5;
+
+    filaBarco1x2_1 = rand()%5;
+    columnaBarco1x2_1 = rand()%6;
+    
+    filaBarco2x1_2 = rand()%6;
+    columnaBarco2x1_2 = rand()%5;
+
+    filaBarco1x2_2 = rand()%5;
+    columnaBarco1x2_2 = rand()%6;
+    
 
     //Creamos una variable para saber si quedan barcos
     bool quedanBarcos = false;
@@ -111,7 +152,7 @@ int main(){
             disparos[i][j]= 0;
         }
     }
-
+    
     do{
         //Inicializamos el tablero a 0
         for(int i=0;i<6;i++){
@@ -120,34 +161,39 @@ int main(){
             }
         }
 
-        //randomificamos un numero aleatorio
-        aleatorio = rand()%2;
 
-        //si aleatorio es 0, creamos un barco de 3x1
-        if(aleatorio == 0){
-            crearBarco31(tablero, rand()%6, rand()%4);
-        }else{aleatorio == 1;
-            crearBarco13(tablero, rand()%4, rand()%6);
+        //CREACION BARCO 3X1 O 1X3
+        //randomificamos el numero aleatorio a 0 o 1
+        Aleatorio3x1_1x3 = rand()%2;
+    
+        //si aleatorio3x1_1x3 es 0, creamos un barco de 3x1
+        if(Aleatorio3x1_1x3 == 0){
+            crearBarco31(tablero, filaBarco3x1, columnaBarco3x1); 
+        }else{Aleatorio3x1_1x3 == 1;
+            crearBarco13(tablero, filaBarco1x3, columnaBarco1x3);
         }
 
+
+        //CREACION BARCO 2X1 O 1X2
         //randomificamos un numero aleatorio
-        aleatorio = rand()%2;
+        Aleatorio2x1_1x2 = rand()%2;
 
         //si aleatorio es 0, creamos un barco de 2x1
-        if(aleatorio == 0){
-            crearBarco21(tablero, rand()%6, rand()%5);
-        }else{aleatorio == 1;
-            crearBarco12(tablero, rand()%5, rand()%6);
+        if(Aleatorio2x1_1x2 == 0){
+            crearBarco21(tablero, filaBarco2x1_1, columnaBarco2x1_1);
+        }else{Aleatorio2x1_1x2 == 1;
+            crearBarco12(tablero, filaBarco1x2_1, columnaBarco1x2_1);
         }
 
+        //CREACION 2º BARCO 2X1 O 1X2
         //randomificamos un numero aleatorio
-        aleatorio = rand()%2;
+        Aleatorio1x2_2x1 = rand()%2;
 
         //si aleatorio es 0, creamos un barco de 2x1
-        if(aleatorio == 0){
-            crearBarco21(tablero, rand()%6, rand()%5);
-        }else{aleatorio == 1;
-            crearBarco12(tablero, rand()%5, rand()%6);
+        if(Aleatorio1x2_2x1 == 0){
+            crearBarco21(tablero, filaBarco2x1_2, columnaBarco2x1_2);
+        }else{Aleatorio1x2_2x1 == 1; 
+            crearBarco12(tablero, filaBarco1x2_2, columnaBarco1x2_2);
         }
 
         //Comprobamos si hay 7 1s en el tablero
@@ -169,6 +215,38 @@ int main(){
 
     }while(Solapan == true);
     
+    //mostramos todas las posiciones de los barcos
+
+    cout<<"Si el barco de 3 es horizontal/vertical (0 horizontal, 1 vertical): "<<Aleatorio3x1_1x3<<endl; 
+
+    
+    cout<<"Si el barco de 3 es horizontal, la fila es: "<<filaBarco3x1<<endl;
+    cout<<"Si el barco de 3 es horizontal, la columna es: "<<columnaBarco3x1<<endl;
+
+    cout<<"Si el barco de 3 es vertical, la fila es: "<<filaBarco1x3<<endl;
+    cout<<"Si el barco de 3 es vertical, la columna es: "<<columnaBarco1x3<<endl;
+
+
+    
+    cout<<"Si el primer barco es de 2 es horizontal/vertical (0 horizontal, 1 vertical): "<<Aleatorio2x1_1x2<<endl;
+    
+    cout<<"Si el primer barco de 2 es horizontal, la fila es: "<<filaBarco2x1_1<<endl;
+    cout<<"Si el primer barco de 2 es horizontal, la columna es: "<<columnaBarco2x1_1<<endl;
+
+    cout<<"Si el primer barco de 2 es vertical, la fila es: "<<filaBarco1x2_1<<endl;
+    cout<<"Si el primer barco de 2 es vertical, la columna es: "<<columnaBarco1x2_1<<endl;
+
+
+
+    cout<<"Si el segundo barco es de 2 es horizontal/vertical (0 horizontal, 1 vertical): "<<Aleatorio1x2_2x1<<endl;
+
+    cout<<"Si el segundo barco de 2 es horizontal, la fila es: "<<filaBarco2x1_2<<endl;
+    cout<<"Si el segundo barco de 2 es horizontal, la columna es: "<<columnaBarco2x1_2<<endl;
+
+    cout<<"Si el segundo barco de 2 es vertical, la fila es: "<<filaBarco1x2_2<<endl;
+    cout<<"Si el segundo barco de 2 es vertical, la columna es: "<<columnaBarco1x2_2<<endl;
+
+
 
     //Mostramos el tablero
     cout << "Mostar tablero con barcos" << endl;
@@ -176,8 +254,8 @@ int main(){
 
     //Empezamos a disparar
         
-    while(disparo(tablero, disparos) == true){
-    disparo(tablero, disparos);
+    while(disparo(tablero) == true){
+    disparo(tablero);
     }
 
     return 0;
